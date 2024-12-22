bd215babce2b76e2004c894be17a271dedf6b5cc
rifkyrofiqq <rifkyrofiq01@gmail.com>
2024-12-22 13:05:44 +0700

    Create Main.java
    Memperbarui file Main untuk inisialisasi aplikasi

---
+++ src/main/java/com/invelistwarehouseinventory/Main.java
@@ -0,0 +1,16 @@
+package com.invelistwarehouseinventory;
+
+import com.invelistwarehouseinventory.Database.Login;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+}
